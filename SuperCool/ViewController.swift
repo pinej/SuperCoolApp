b8444ed8082b450f08ebd3e36b5aad6dd7c15b74
pinej <piotr.nejman@gmail.com>
2015-12-25 12:32:30 +0100

    Super Cool app from Udemy

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Piotr Nejman on 23.12.2015.
+//  Copyright © 2015 pinej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    @IBOutlet weak var CoolLogo: UIImageView!
+    @IBOutlet weak var CoolBg: UIImageView!
+    @IBOutlet weak var unCoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+    CoolLogo.hidden = false
+        CoolBg.hidden = false
+        unCoolButton.hidden = true
+    }
+}
+
